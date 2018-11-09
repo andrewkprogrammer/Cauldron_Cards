1046ad204c569a82bb8e39cba289a42e61176a44
Sam <sammydeedge@hotmail.com>
2018-11-09 16:53:23 +1100

    Added Basic Spider and Turn Ticker Bar
    Still needs work for Turn Ticker Functionality. Proper spider and Bar assets to be made

---
+++ Cauldron-Cards/Assets/Codes/TurnTick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnTick : MonoBehaviour {
+
+    GameObject elapsedTurnsBar;
+
+    public int Turns_Total;
+    public int Turns_Elapsed;
+
+	// Use this for initialization
+	void Start () {
+        elapsedTurnsBar = GameObject.Find("Turn_Bar_Elapsed");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RectTransform elapsed_Transform = elapsedTurnsBar.GetComponent<RectTransform>();
+        float barwidth = 100 - (Turns_Elapsed / Turns_Total);
+	}
+}
