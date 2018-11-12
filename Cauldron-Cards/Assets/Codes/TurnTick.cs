6c3bcefd98758d10c3b417723d1812f3dbe10fb9
Sam <sammydeedge@hotmail.com>
2018-11-13 09:46:07 +1100

    Added Inventory Screen

--- Cauldron-Cards/Assets/Codes/TurnTick.cs
+++ Cauldron-Cards/Assets/Codes/TurnTick.cs
@@ -1,22 +1,124 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TurnTick : MonoBehaviour {
 
     GameObject elapsedTurnsBar;
+    GameObject spider;
 
+    [Range(5,40)]
     public int Turns_Total;
-    public int Turns_Elapsed;
+     int Turns_Elapsed = 0;
 
-	// Use this for initialization
-	void Start () {
+    RectTransform turnBar_RT;
+
+    public float speed;
+
+    Vector2 SPDR_start_pos;
+    Vector2 SPDR_end_pos;
+
+    bool isTurnMoving = false;
+
+    float current_percent;
+    float prev_percent;
+    float t = 0;
+
+
+    public enum EventType
+    {
+        GoodEvent,
+        BadEvent,
+        GameOver,
+        Null
+    }
+
+    public Dictionary<int, EventType> m_eventList = new Dictionary<int, EventType>();
+
+    // Use this for initialization
+    void Start () {
         elapsedTurnsBar = GameObject.Find("Turn_Bar_Elapsed");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        RectTransform elapsed_Transform = elapsedTurnsBar.GetComponent<RectTransform>();
-        float barwidth = 100 - (Turns_Elapsed / Turns_Total);
-	}
+        for (int i = 0; i < Turns_Total; i++)
+        {
+            m_eventList[i] = EventType.Null;
+        }
+        m_eventList[Turns_Total] = EventType.GameOver;
+        spider = GameObject.Find("SpiderObject");
+        turnBar_RT = elapsedTurnsBar.GetComponent<RectTransform>();
+
+        SPDR_start_pos = GameObject.Find("SpiderStart").transform.localPosition;
+        SPDR_end_pos = GameObject.Find("SpiderEnd").transform.localPosition;
+    }
+
+
+    void Update()
+    {
+        if (isTurnMoving)
+        {
+
+
+            //Returns start and end position 
+
+
+            Vector2 next_pos = Vector2.Lerp(SPDR_start_pos, SPDR_end_pos, current_percent);
+            spider.transform.localPosition = Vector2.MoveTowards(spider.transform.localPosition, next_pos, speed);
+            
+
+            prev_percent = ((float)(Turns_Elapsed - 1) / (float)Turns_Total);
+
+            t += speed * Time.deltaTime;
+            float scaling = Mathf.Lerp(prev_percent, current_percent, t);
+            turnBar_RT.sizeDelta = new Vector2(scaling * 100, 100);
+            if (t >= 1.0f)
+            {
+                isTurnMoving = false;
+                t = 0.0f;
+                EventType eventType = m_eventList[Turns_Elapsed];
+                if (eventType == EventType.GameOver)
+                {
+                    runGameOver();
+                }
+                else if (eventType == EventType.GoodEvent)
+                {
+                    runGoodEvent();
+                }
+                else if (eventType == EventType.BadEvent)
+                {
+                    runBadEvent();
+                }
+            }
+        }
+    }
+
+    void updateTurnVisuals()
+    {
+        //Gets percentage of completion
+        current_percent = ((float)Turns_Elapsed / (float)Turns_Total);
+        prev_percent = ((float)(Turns_Elapsed - 1) / (float)Turns_Total);
+    }
+
+    public void onTurnTick()
+    {
+        Turns_Elapsed++;
+        isTurnMoving = true;
+        
+        //Other if statements to implement Good and Bad Events
+        updateTurnVisuals();
+    }
+
+    void runGameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+    void runGoodEvent()
+    {
+
+    }
+
+    void runBadEvent()
+    {
+
+    }
 }
---
+++ Cauldron-Cards/Assets/Inven_Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inven_Test : MonoBehaviour {
+
+    CardController CardControls;
+
+    void Start()
+    {
+        CardControls = GameObject.Find("CardController").GetComponent<CardController>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (Input.GetKeyDown(KeyCode.R))
+        {
+            CardControls.addARedCard();
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            CardControls.addABlueCard();
+        }
+    }
+}
