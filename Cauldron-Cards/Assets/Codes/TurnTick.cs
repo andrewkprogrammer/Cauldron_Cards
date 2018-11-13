d22b805989a6875d556f63520e12c9c6d6191a17
Sam <sammydeedge@hotmail.com>
2018-11-13 11:02:58 +1100

    Event processing completed and Tested
    Will be refactoring code

--- Cauldron-Cards/Assets/Codes/TurnTick.cs
+++ Cauldron-Cards/Assets/Codes/TurnTick.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TurnTick : MonoBehaviour {
 
@@ -25,6 +26,8 @@ public class TurnTick : MonoBehaviour {
     float prev_percent;
     float t = 0;
 
+    Text eventTestText;
+
 
     public enum EventType
     {
@@ -44,11 +47,14 @@ public class TurnTick : MonoBehaviour {
             m_eventList[i] = EventType.Null;
         }
         m_eventList[Turns_Total] = EventType.GameOver;
+        m_eventList[3] = EventType.GoodEvent;
+        m_eventList[5] = EventType.BadEvent;
         spider = GameObject.Find("SpiderObject");
         turnBar_RT = elapsedTurnsBar.GetComponent<RectTransform>();
 
         SPDR_start_pos = GameObject.Find("SpiderStart").transform.position;
         SPDR_end_pos = GameObject.Find("SpiderEnd").transform.position;
+        eventTestText = GameObject.Find("Event Tests").GetComponent<Text>();
     }
 
 
@@ -60,16 +66,22 @@ public class TurnTick : MonoBehaviour {
 
             //Returns start and end position 
 
-
+            //Finds the location of the next node to reach
             Vector2 next_pos = Vector2.Lerp(SPDR_start_pos, SPDR_end_pos, current_percent);
-             spider.transform.localPosition = Vector2.MoveTowards(spider.transform.position, next_pos, speed);
+
+            //Moves spider towards the next node
+            spider.transform.position = Vector2.MoveTowards(spider.transform.position, next_pos, speed);
             
 
+            //Gets the previous node
             prev_percent = ((float)(Turns_Elapsed - 1) / (float)Turns_Total);
 
+            //Scaling up the bar
             t += speed * Time.deltaTime * 1.6f;
             float scaling = Mathf.Lerp(prev_percent, current_percent, t);
             turnBar_RT.sizeDelta = new Vector2(scaling * 100, 100);
+
+            //When turn is complete, reset all the pre-turning parameters, and run any events needed
             if (t >= 1.0f)
             {
                 isTurnMoving = false;
@@ -87,6 +99,7 @@ public class TurnTick : MonoBehaviour {
                 {
                     runBadEvent();
                 }
+
             }
         }
     }
@@ -114,11 +127,11 @@ public class TurnTick : MonoBehaviour {
 
     void runGoodEvent()
     {
-
+        eventTestText.text = "Good Event";
     }
 
     void runBadEvent()
     {
-
+        eventTestText.text = "Bad Event";
     }
 }
