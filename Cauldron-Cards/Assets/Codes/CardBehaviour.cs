060cc9b3896f44e153a3418ef7de02d2f84b30b1
Andrew Kenny <andrewckenny97@gmail.com>
2018-11-16 14:46:52 +1100

    Cleaned Up Code

---
+++ Cauldron-Cards/Assets/Codes/CardBehaviour.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+
+public class CardBehaviour : MonoBehaviour, IPointerClickHandler
+ {
+    [Tooltip("The time before the card flips back to it's original position \n (for when the character matches the wrong cards)")]
+    [Range(0.0f, 10.0f)]
+    //float flipTime = 3.0f;
+    
+
+    static CardInfo cardInfo;
+
+    bool Front_Showing = false;
+    //float timer = 0.0f;
+
+    public CardInfo.CardColour ThisColour;
+
+
+    Animator animator;
+
+    void Start()
+    {
+        if (cardInfo == null)
+        {
+            cardInfo = new CardInfo();
+        }
+        animator = GetComponent<Animator>();
+        ThisColour = CardInfo.CardColour.White;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+     {
+        if (!Front_Showing)
+        {
+            animator.SetTrigger("Display");
+            Front_Showing = true;
+            cardInfo.AddCard(ThisColour, this);
+        }
+        //else
+        //{
+        //    animator.SetTrigger("Reset");
+        //    Front_Showing = false;
+        //}
+     }
+
+     // Use this for initialization
+
+
+     // Update is called once per frame
+     void Update()
+     {
+        //if(Front_Showing && neededColour != selectedColour && animator.GetCurrentAnimatorStateInfo(0).IsName("Idle_Forward"))
+        //{
+        //
+        //    foreach(CardBehaviour card in clickedCards)
+        //    {
+        //        card.unflip();
+        //    }
+        //    clickedCards.Clear();
+        //}
+        if(ThisColour == CardInfo.CardColour.White)
+        {
+            ThisColour = cardInfo.GiveColour();
+        }
+
+        cardInfo.Update();
+
+     }
+
+    public void unflip()
+    {
+        animator.SetTrigger("Reset");
+        Front_Showing = false;
+    }
+
+    //bool IsListColourAtExcess(CardInfo.CardColour colour)
+    //{
+    //    int numInList = 0;
+    //
+    //    foreach(CardBehaviour card in clickedCards)
+    //    {
+    //        if (card.ThisColour == colour)
+    //            numInList++;
+    //    }
+    //    if (numInList > 1)
+    //        return true;
+    //    return false;
+    //}
+ }
--- Cauldron-Cards/Assets/Codes/CardInfo.cs
+++ Cauldron-Cards/Assets/Codes/CardInfo.cs
@@ -3,30 +3,100 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class CardInfo : MonoBehaviour
+public class CardInfo
 {
-    
-    public enum CardColour
+
+    public enum CardColour : int
     {
-        red, blue, green, yellow
+        Red = 1,
+        Blue = 2,
+        Yellow = 3,
+        Green = 4,
+
+        White = 0 //white exists as a type of "null" value
     }
-    CardColour selectedColour;
+
+    const int AmountOfEachColour = 4;
+    int numberOfClickedCards = 0;
+
+    List<CardBehaviour> clickedCards;
+    List<CardColour> ColourPool;
+    CardColour SelectedColour;
+    CardColour[] colours = { CardColour.Red, CardColour.Blue, CardColour.Yellow, CardColour.Green }; // a pool of colours to give to cards
+
     // Use this for initialization
-    void Start()
+    public CardInfo()
     {
+        ColourPool = new List<CardColour>();
+        for (int i = 0; i < 4; i++)
+        {
+            for(int j = 0; j < AmountOfEachColour; j++)
+            {
+                ColourPool.Add(colours[i]);
+            }
+        }
+
+        clickedCards = new List<CardBehaviour>();
 
     }
+
+
+
+
+//_________________________________________________________________________________
+
+
+    //  adds a card to the list of cards, typically this should only consist of two cards at most
+    public void AddCard(CardColour colour, CardBehaviour sender) 
+    {
+        numberOfClickedCards++;
+        SelectedColour = colour;
+        clickedCards.Add(sender);
+    }
+
+    //  each card uses this function when it's made to decide what colour it should be
+    //  without having too many of the same colour
+    public CardColour GiveColour()
+    {
+        if (ColourPool.Count > 0)
+        {
+            int randnum = Random.Range(0, ColourPool.Count);
+            CardColour returnColour = ColourPool[randnum];
+            ColourPool.RemoveAt(randnum);
+            return returnColour;
+        }
+        else
+        {
+            return CardColour.White;
+        }
+    }
+
+    //  checks the two selected cards and returns true if they are the same colour
+    private bool CardsAreSame()
+    {
+        if (clickedCards[0].ThisColour != clickedCards[1].ThisColour)
+            return false;
+        else
+            return true;
+    }
+//_________________________________________________________________________________
+
+
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        if(clickedCards.Count == 2 && !CardsAreSame())
+        {
+            foreach(CardBehaviour card in clickedCards)
+            {
+                card.unflip();
+            }
+            clickedCards.Clear();
+        }
+        else if(clickedCards.Count == 2 && CardsAreSame())
+        {
+            clickedCards.Clear();
+        }
     }
-
-    public void ChangeColour(CardColour colour)
-    {
-        selectedColour = colour;
-    }
-
-
 }
