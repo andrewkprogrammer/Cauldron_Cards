7d8dce04e8368e1807911389687b9d6d05f7bb94
Sam <sammydeedge@hotmail.com>
2018-11-27 16:46:45 +1100

    Fixing Visuals

---
+++ Cauldron-Cards/Assets/Codes/MusicControls.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicControls : MonoBehaviour {
+
+    SoundTransitionTrigger emitter;
+    public float fadeOutTime;
+
+    public string nextSceneName;
+
+    SceneLoaderBehaviour sceneLoader;
+
+	// Use this for initialization
+	void Start () {
+        emitter = GetComponent<SoundTransitionTrigger>();
+        sceneLoader = GameObject.Find("SceneLoader").GetComponent<SceneLoaderBehaviour>();
+	}
+
+
+    // Update is called once per frame
+    public void turnOff () {
+        emitter.setNextSceneName(nextSceneName);
+        emitter.fadeOutVolume(fadeOutTime);
+	}
+    public void turnOff(string sceneName)
+    {
+        emitter.setNextSceneName(sceneName);
+        emitter.fadeOutVolume(fadeOutTime);
+    }
+}
