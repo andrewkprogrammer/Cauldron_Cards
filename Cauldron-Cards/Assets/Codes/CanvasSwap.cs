5a25f7d55ee7b8b7a7397f33a4c76cf8c2c2e0bd
Sam <sammydeedge@hotmail.com>
2018-11-29 11:06:15 +1100

    Updated Menu/UI Visuals

--- Cauldron-Cards/Assets/Codes/CanvasSwap.cs
+++ Cauldron-Cards/Assets/Codes/CanvasSwap.cs
@@ -5,26 +5,36 @@ using UnityEngine;
 public class CanvasSwap : MonoBehaviour {
 
     public Canvas canvas_start;
-    bool cnvStart_enabled;
 
-    public Canvas canvas_end;
-    bool cnvEnd_enabled;
+    public Canvas canvas_second;
+
+    public Canvas canvas_third;
+
+    bool[] enabledList = new bool[3] { true, false, false };
 
     // Use this for initialization
     void Start () {
-        cnvStart_enabled = true;
-        cnvEnd_enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        canvas_start.enabled = cnvStart_enabled;
-        canvas_end.enabled = cnvEnd_enabled;
+        canvas_start.enabled    = enabledList[0];
+        canvas_second.enabled   = enabledList[1];
+        canvas_third.enabled    = enabledList[2];
 	}
 
-    public void swapCanvas()
+    public void swapToCanvas(int num)
     {
-        cnvEnd_enabled = !cnvEnd_enabled;
-        cnvStart_enabled = !cnvStart_enabled;
+        for (int i = 0; i < enabledList.Length; ++i)
+        {
+            if (i == num)
+            {
+                enabledList[i] = true;
+            }
+            else
+            {
+                enabledList[i] = false;
+            }
+        }
     }
 }
