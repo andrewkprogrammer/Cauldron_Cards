bdf9422be29f94de36fd894b97cc5cc9f720bbec
Sam <sammydeedge@hotmail.com>
2018-11-29 17:37:06 +1100

    Added cutscene

---
+++ Cauldron-Cards/Assets/CutsceneEndCheck.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class CutsceneEndCheck : MonoBehaviour {
+
+    VideoPlayer cutscene;
+
+    SceneLoaderBehaviour sceneLoader;
+    public string nextSceneName;
+
+    float time = 0;
+
+    private void Start()
+    {
+        cutscene = GameObject.Find("CutSceneVideo").GetComponent<VideoPlayer>();
+        sceneLoader = GameObject.Find("SceneLoader").GetComponent<SceneLoaderBehaviour>();
+    }
+
+    private void LateUpdate()
+    {
+        time += Time.deltaTime;
+        if (time > 1.0f && !cutscene.isPlaying)
+        {
+            sceneLoader.loadScene(nextSceneName);
+        }
+    }
+
+}
